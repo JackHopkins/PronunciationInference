2b12d896696c85c23045978d8c5bc649625a559c
jack <jack.hopkins@me.com>
2015-12-25 21:19:34 +0000

    l

--- PronunciationInference/aws-deploy.sh
+++ PronunciationInference/aws-deploy.sh
@@ -5,5 +5,4 @@ git push origin master
 
 ssh -i /Users/jack/Downloads/mac-key.pem ubuntu@ec2-54-229-5-172.eu-west-1.compute.amazonaws.com 'sudo rm -rf PronunciationInference'
 ssh -i /Users/jack/Downloads/mac-key.pem ubuntu@ec2-54-229-5-172.eu-west-1.compute.amazonaws.com 'sudo git clone https://github.com/JackHopkins/PronunciationInference.git'
-ssh -i /Users/jack/Downloads/mac-key.pem ubuntu@ec2-54-229-5-172.eu-west-1.compute.amazonaws.com 'cd PronunciationInference/PronunciationInference/'
-ssh -i /Users/jack/Downloads/mac-key.pem ubuntu@ec2-54-229-5-172.eu-west-1.compute.amazonaws.com 'sudo java -jar ./PronunciationInference.jar'
+ssh -i /Users/jack/Downloads/mac-key.pem ubuntu@ec2-54-229-5-172.eu-west-1.compute.amazonaws.com 'sudo java -jar ./PronunciationInference/PronunciationInference/PronunciationInference.jar'
